86f9db2420ce11bd0d197da0a30d4154f949322a
Gordon Sun <gsun1729@gmail.com>
2023-06-30 19:50:30 -0400

    added ID for particles, to increment with creation

--- include/objects.hpp
+++ include/objects.hpp
@@ -5,12 +5,16 @@ class Particle {
     double p_reaction_;
     double position_;
     double diff_coeff_;
+    static int next_id;
+    int id_;
 public:
     Particle();
     Particle(double pos, double diff_coeff, double p_reaction);
+    int getId() const { return id_; }
     // move the particle according to its diffusion coefficient
     void Move(double dt); 
-    // make the particle blink with a certain probability
+    
+    
     bool HasReacted(); 
 
     double getPosition() const;
