125b90d2f714e4aaccb913b26868a0a802fcd41e
Gordon Sun <gsun1729@gmail.com>
2023-06-30 19:50:10 -0400

    mv i/oflux to be handled by axis class handleHoles function, added more attributes to classes

--- include/axis.hpp
+++ include/axis.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include "objects.hpp"
 #include <vector>
-
+#include <algorithm>
+#include <memory>
 
 class Hole {
     double position_;
@@ -13,25 +14,29 @@ public:
     Hole(double pos, double width, double influxProb, double efluxProb);
     // check if a given particle is inside this hole
     bool isParticleInside(const Particle& p); 
-    // determine if a particle in this hole should exit the axis
-    bool shouldExit(); 
-    // determine if a new particle should enter the axis
-    bool shouldInflux(); 
+    float getCenterPosition() const;
+    float getRandomPosition() const;
+    float getEFluxProb() const;
+    float getInFluxProb() const;
+
 };
 
 
 
 
 class Axis {
-    double length;
-    std::vector<Hole> holes;
-    std::vector<Particle> particles;
+    double length_;
+    double start_position_;
+    double end_position_;
+    std::vector<Hole> hole_list_;
+    std::vector<std::shared_ptr<Particle>> particle_list_;
 public:
     Axis(double length);
     void addHole(const Hole& hole);
     void addParticle(const Particle& particle);
+    void rmParticle(const Particle& particle);
     // move all particles along the axis
-    void moveParticles(); 
+    void moveParticles(double dt); 
     // handle influx/eflux of particles in each hole
     void handleHoles(); 
 };
