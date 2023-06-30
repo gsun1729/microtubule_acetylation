20db3d20bd073d1dc8615fdc52e18260ad5168a9
Gordon Sun <gsun1729@gmail.com>
2023-06-30 19:51:00 -0400

    testing of particle generation, IDs, and movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,64 @@
-#include<iostream>
+#include <iostream>
 #include "probability.hpp"
 #include "config.hpp"
 #include "objects.hpp"
 #include "axis.hpp"
 #include <stdexcept>
 #include <iomanip>
+#include <chrono>
+#include <thread>
+#include <stdlib.h>
+class Simulation
+{
+public:
+   void test()
+   {
+      Hole H = Hole(0, 0.5, 0, 0);
+      float rand_pos = rand() / double(RAND_MAX);
+      Particle p = Particle(rand_pos, 0.1, 0.1);
 
+      bool p_inside_H = H.isParticleInside(p);
+      std::cout << "in\t" << p_inside_H << std::endl;
+   }
+};
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
    // std::cout << config.diffusion_coefficient << std::endl;
 
-   std::cout << "====="<<std::endl;
+   std::cout << "=====" << std::endl;
 
-   Hole H = Hole(0, 1, 0,0);
+   std::vector<std::shared_ptr<Particle>> particles(10);
 
-   Particle p = Particle(2, 0.1, 0.1);
+   srand(time(NULL));
+   float rand_pos;
+   for (auto &particle : particles)
+   {
+      rand_pos = rand() / double(RAND_MAX);
+      particle = std::make_shared<Particle>(rand_pos, 0.1, 0.1);
+      std::cout << particle->getId() << "\t" << particle->getPosition() << std::endl;
+   }
 
-   bool p_inside_H = H.isParticleInside(p);
-   std::cout<<p_inside_H<<std::endl;
-
-   for (int i=0; i<10; i++){
-      p.PrintSelf();
-      p.Move(0.1);
-}
-
-
-   // std::cout << std::setprecision(5)<< std::fixed;
-   // // }
-   // float axis_len = 100;
-   // float width = 1;
-   // int n_holes = 5;
-   // if (width * n_holes > axis_len){
-   //    throw std::invalid_argument("axis cannot be all holes");
-   // }
-
-   // std::cout<<"Separation\t"<<axis_len/(n_holes+1)<<std::endl;
-   // for (int i=0; i<n_holes; i++){
-   //    float position = (axis_len/(n_holes+1))*(i+1);
-   //    std::cout<<position<<"\t"<<position-width/2<<"\t"<<position + width/2<<std::endl;
-   // }
-
-
-
+   for (auto &particle : particles)
+   {
+      particle->Move(0.1);
+      std::cout << particle->getPosition() << std::endl;
+   }
 
    return 0;
-   
+   // std::vector<std::thread> threads;
+
+   // for (int i = 0; i <= 5; i++)
+   // {
+   //    Simulation *sim = new Simulation();
+
+   //    threads.push_back(std::thread(&Simulation::test, sim));
+   // }
+
+   // for (auto &thread : threads)
+   // {
+   //    thread.join();
+   // }
+
+   return 0;
 }
