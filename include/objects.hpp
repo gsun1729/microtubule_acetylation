f434ecf2de9140fc62ddf0dd2909074c6564d876
Gordon Sun <gsun1729@gmail.com>
2023-07-04 11:42:15 -0400

    mv Hole and Axis classes into objects library

--- include/objects.hpp
+++ include/objects.hpp
@@ -1,26 +1,78 @@
+#include <vector>
+#include <algorithm>
+#include <memory>
 #pragma once
 
-class Particle {
+class Particle
+{
     float p_reaction_;
     float position_;
     float diff_coeff_;
     static int next_id_;
     int id_;
+
 public:
     Particle();
     Particle(float pos, float diff_coeff, float p_reaction);
     int getId() const { return id_; }
     // move the particle according to its diffusion coefficient
-    void Move(float dt); 
-    
-    
-    bool HasReacted(); 
-
+    void Move(float dt);
+    bool HasReacted();
     float getPosition() const;
-
     float getDiffusionCoeff() const;
     float getRxnProbability() const;
-
-    void PrintSelf() const;
+    void printSelf() const;
 };
 
+class Hole
+{
+    float position_;
+    float width_;
+    float start_pos_, end_pos_;
+    float influxProbability_;
+    float efluxProbability_;
+
+public:
+    Hole(float pos, float width, float influxProb, float efluxProb);
+    // check if a given particle is inside this hole
+    bool isParticleInside(const Particle &p);
+    float getCenterPosition() const;
+    float getRandomPosition() const;
+    float getEFluxProb() const;
+    float getInFluxProb() const;
+    void printPosition() const;
+    void printProbability() const;
+    void printSelf() const;
+};
+
+class Axis
+{
+    float length_;
+    float start_position_;
+    float end_position_;
+    float center_position_{0};
+
+    int num_holes_{-1};
+    float hole_width_{-1};
+    float hole_separation_{-1};
+
+    std::vector<Hole> hole_list_;
+    std::vector<std::shared_ptr<Particle>> particle_list_;
+
+public:
+    Axis();
+    Axis(float start_pos, float end_pos);
+    void addHole(const Hole &hole);
+    void addHoles(int num_holes, float hole_width);
+    void addHoles(float hole_width, float hole_separation, float seed_pt);
+    void addParticle(const Particle &particle);
+    void rmParticle(const Particle &particle);
+    // move all particles along the axis
+    void moveParticles(float dt);
+    // handle influx/eflux of particles in each hole
+    void handleHoles();
+    void printDimensions() const;
+    void printHoleLocations() const;
+    void printParticlePositions() const;
+    void printAcetylationPoints();
+};
--- src/objects.cpp
+++ src/objects.cpp
@@ -1,8 +1,6 @@
 #include "objects.hpp"
 #include "config.hpp"
 #include "probability.hpp"
-#include "config.hpp"
-
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -19,8 +17,6 @@ Particle::Particle(){
 	position_ = 0;
 	diff_coeff_ = config.diffusion_coefficient;
 	p_reaction_ = config.p_reaction;
-	
-
 }
 
 
@@ -46,8 +42,6 @@ bool Particle::HasReacted(){
 	return isRandomSampleGreaterThanValue(p_reaction_);
 }
 
-
-
 float Particle::getPosition() const{
 	return position_;
 }
@@ -60,9 +54,229 @@ float Particle::getRxnProbability() const{
 	return p_reaction_;
 }
 
-void Particle::PrintSelf() const{
+void Particle::printSelf() const{
 	std::cout << std::setprecision(5)<< std::fixed;
 	std::cout << "position: " << position_ << "\t";
 	std::cout << "D: "<< diff_coeff_ << "\t";
 	std::cout << "pRx: "<< p_reaction_ << std::endl;
+}
+
+
+
+
+Hole::Hole(float pos, float width,
+		   float influxProb, float efluxProb)
+{
+	position_ = pos;
+	width_ = width;
+	influxProbability_ = influxProb;
+	efluxProbability_ = efluxProb;
+	start_pos_ = pos - (width / 2.0);
+	end_pos_ = pos + (width / 2.0);
+}
+
+bool Hole::isParticleInside(const Particle &p)
+{
+	float particle_pos = p.getPosition();
+	if (particle_pos >= start_pos_ && particle_pos <= end_pos_)
+	{
+		return (true);
+	}
+	else
+	{
+		return (false);
+	}
+}
+
+float Hole::getCenterPosition() const
+{
+	return position_;
+}
+
+float Hole::getRandomPosition() const
+{
+	float pos_ratio = sampleUniformDistribution();
+	return (width_ * pos_ratio + start_pos_);
+}
+
+float Hole::getEFluxProb() const
+{
+	return efluxProbability_;
+}
+
+float Hole::getInFluxProb() const
+{
+	return influxProbability_;
+}
+
+void Hole::printPosition() const
+{
+	std::cout << "pos\t" << position_ << std::endl;
+	std::cout << "range\t" << start_pos_ << "\t" << end_pos_ << std::endl;
+}
+
+void Hole::printProbability() const
+{
+	std::cout << "in\t" << influxProbability_ << "out\t" << efluxProbability_ << std::endl;
+}
+
+void Hole::printSelf() const
+{
+	std::cout << std::setprecision(2) << std::fixed;
+	std::cout << "pos\t" << position_ << "\t";
+	std::cout << "width\t" << width_ << "\t";
+	std::cout << "pIN\t" << influxProbability_ << "\t";
+	std::cout << "pOUT\t" << efluxProbability_ << "\n";
+}
+
+
+Axis::Axis()
+{
+	length_ = config.axis_len;
+	start_position_ = center_position_ - length_ / 2.0;
+	end_position_ = center_position_ + length_ / 2.0;
+}
+
+Axis::Axis(float start_pos, float end_pos)
+{
+	length_ = end_pos - start_pos;
+	center_position_ = (start_pos + end_pos) / 2.0;
+	start_position_ = start_pos;
+	end_position_ = end_pos;
+}
+
+void Axis::addHole(const Hole &hole)
+{
+	hole_list_.push_back(hole);
+}
+
+void Axis::addHoles(int num_holes, float hole_width)
+{
+	if (num_holes * hole_width > length_)
+	{
+		std::cout << "Number of holes and width exceeds length of axis" << std::endl;
+		exit(0);
+	}
+	num_holes_ = num_holes;
+	hole_width_ = hole_width;
+
+	for (int i = 0; i < num_holes_; i++)
+	{
+		Hole newHole(start_position_ + (length_ * (i + 1) / (num_holes_ + 1)),
+					 config.divider_width,
+					 config.p_entry,
+					 config.p_exit);
+		this->addHole(newHole);
+	}
+}
+
+void Axis::addHoles(float hole_width, float hole_separation, float seed_pos)
+{
+	hole_width_ = hole_width;
+	hole_separation_ = hole_separation;
+
+	float adder = 0.0;
+	bool reach_front = false, reach_end = false;
+	Hole newHole(seed_pos,
+				 config.divider_width,
+				 config.p_entry,
+				 config.p_exit);
+	this->addHole(newHole);
+	adder += hole_width_ + hole_separation_;
+
+	while (!(reach_front && reach_end))
+	{
+		reach_end = (seed_pos + adder + hole_width_ / 2) > end_position_;
+		reach_front = (seed_pos - adder - hole_width_ / 2) < start_position_;
+		if (!reach_front)
+		{
+			Hole newHole(seed_pos - adder,
+						 config.divider_width,
+						 config.p_entry,
+						 config.p_exit);
+			this->addHole(newHole);
+		}
+		if (!reach_end)
+		{
+			Hole newHole(seed_pos + adder,
+						 config.divider_width,
+						 config.p_entry,
+						 config.p_exit);
+			this->addHole(newHole);
+		}
+		adder += hole_width_ + hole_separation_;
+	}
+}
+
+void Axis::addParticle(const Particle &particle)
+{
+	particle_list_.push_back(std::make_shared<Particle>(particle));
+}
+
+void Axis::rmParticle(const Particle &particle)
+{
+	particle_list_.erase(std::remove_if(particle_list_.begin(), particle_list_.end(),
+										[&](const std::shared_ptr<Particle> &p)
+										{
+											return p->getId() == particle.getId();
+										}),
+						 particle_list_.end());
+}
+
+void Axis::moveParticles(float dt)
+{
+	for (auto &p : particle_list_)
+	{
+		p->Move(dt);
+	}
+}
+
+void Axis::handleHoles()
+{
+	for (auto &hole : hole_list_)
+	{
+		if (isRandomSampleGreaterThanValue(hole.getInFluxProb()))
+		{
+			addParticle(Particle(hole.getRandomPosition(),
+								 /*diff_coeff=*/config.diffusion_coefficient,
+								 /*p_reaction=*/config.p_reaction));
+		}
+
+		for (auto it = particle_list_.begin(); it != particle_list_.end();)
+		{
+			auto &p = *it;
+			if (hole.isParticleInside(*p) && isRandomSampleGreaterThanValue(hole.getEFluxProb()))
+			{
+				it = particle_list_.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+		}
+	}
+}
+
+void Axis::printDimensions() const
+{
+	std::cout << "length\t" << length_ << std::endl;
+	std::cout << "start\t" << start_position_ << std::endl;
+	std::cout << "end\t" << end_position_ << std::endl;
+	std::cout << "center\t" << center_position_ << std::endl;
+}
+
+void Axis::printHoleLocations() const
+{
+	for (const auto &h : hole_list_)
+	{
+		h.printSelf();
+	}
+}
+
+void Axis::printParticlePositions() const
+{
+	for (const auto &p : particle_list_)
+	{
+		p->printSelf();
+	}
 }
