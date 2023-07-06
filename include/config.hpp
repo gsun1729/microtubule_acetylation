35efd4031ba1b333a959813929a95fa257dad2d3
Gordon Sun <gsun1729@gmail.com>
2023-07-06 01:29:39 -0400

    added args for filename

--- include/config.hpp
+++ include/config.hpp
@@ -1,14 +1,18 @@
-#pragma	once
+#include <string>
+#pragma once
 
-struct Config {
-	const float microtubule_len = 0.1;
-	const float diffusion_coefficient = 0.23;
-	const float time_step = 0.1;
-	const float p_entry = 0.1;
-	const float p_exit = 0.1;
-	const float p_reaction = 0.243;
-	const float divider_width = 0.1;
-	const int num_dividers = 3;
+struct Config
+{
+    const std::string filename = "testing_runs";
+    const float axis_len = 10;                // microtubule length in um
+    const float diffusion_coefficient = 0.27; // aTAT1 diffusion 1D um2/s
+    const float time_step = 0.01;
+    const float p_entry = 0.00005;
+    const float p_exit = 0.00005;
+    const float p_reaction = 0.0000003;
+    const float divider_width = 0.015; // defect size 10-20nm
+    const int num_dividers = 3;
+    const float stop_time = 300.0; // seconds
 };
 
 extern const Config config;
