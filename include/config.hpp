c148dd5c1e508ce13fad666edadbfbc5d7ebf05a
Gordon Sun <gsun1729@gmail.com>
2023-07-06 18:41:04 -0400

    converted configs to non-const for updatability

--- include/config.hpp
+++ include/config.hpp
@@ -3,16 +3,23 @@
 
 struct Config
 {
-    const std::string filename = "testing_runs";
-    const float axis_len = 10;                // microtubule length in um
-    const float diffusion_coefficient = 0.27; // aTAT1 diffusion 1D um2/s
-    const float time_step = 0.01;
-    const float p_entry = 0.00005;
-    const float p_exit = 0.00005;
-    const float p_reaction = 0.0000003;
-    const float divider_width = 0.015; // defect size 10-20nm
-    const int num_dividers = 3;
-    const float stop_time = 300.0; // seconds
-};
+    std::string filename = "testing_runs";
+    std::string save_dir = "experiments";
+    float axis_len = 50;                // microtubule length in um
+    float diffusion_coefficient = 0.27; // aTAT1 diffusion 1D um2/s
+    float timestep_size = 0.001;
+    float stop_time = 300.0; // seconds
+    float record_per_s = 60.0;
+    bool record_frames{true};
 
-extern const Config config;
+
+    //Modifiable parameters
+    float p_entry = 0.05;
+    float p_exit = 0.05;
+
+    float p_reaction = 0.03;
+    float hole_width = 0.015; // defect size 10-20nm
+    int num_holes = 3;
+
+    float precision_epsilon = 0.001;
+};
