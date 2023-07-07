9a7002a6b00cf3ccda9bbc386c4b396b75e35c87
Gordon Sun <gsun1729@gmail.com>
2023-07-06 21:19:26 -0400

    baseline parameters

--- include/config.hpp
+++ include/config.hpp
@@ -7,20 +7,20 @@ struct Config
     std::string save_dir = "experiments";
     float axis_len = 50;                // microtubule length in um
     float diffusion_coefficient = 0.27; // aTAT1 diffusion 1D um2/s
-    float timestep_size = 0.1;
-    float stop_time = 3.0; // seconds
-    float record_per_s = 1.0;
+    float timestep_size = 0.001;
+    float stop_time = 300.0; // seconds
+    float record_per_s = 60.0;
     bool record_frames{true};
 
     // Modifiable parameters
-    float p_entry = 0.05;
-    float p_exit = 0.05;
+    float p_entry = 0.000005; //per frame
+    float p_exit = 0.000005; //per frame
 
-    float p_reaction = 0.03;
+    float p_reaction = 0.00001; //per frame
     float hole_width = 0.015; // defect size 10-20nm
     int num_holes = 3;
 
-    float precision_epsilon = 0.1;
+    float precision_epsilon = 0.001;
     int simulation_number = 0;
     
 };
